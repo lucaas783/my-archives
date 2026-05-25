b34259587c6f58ce16c7a492574e1168604e0fa8
lucaas783 <gdlucas10@gmail.com>
2026-05-25 19:01:18 -0300

    Removi a pasta dos projetos

--- codigos-Py/projeto1p-Py/teste_modular1.py
+++
@@ -1,8 +0,0 @@
-Carros = {
-    "Toyota": ["Corolla", "Hilux", "Camry", "Prius"],
-    "Honda": ["Civic", "Accord", "CR-V", "Fit"],
-    "Ford": ["Fiesta", "Focus", "Mustang", "Explorer"],
-    "Chevrolet": ["Onix", "Cruze", "S10", "Camaro"],
-    "Volkswagen": ["Gol", "Polo", "Golf", "Passat"],
-    "Mercedes": ["E63", "A110", "C63", "SLS"],
-}
